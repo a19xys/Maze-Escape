e71dcf2cce825355ba6f612b6aca06f58edcdd06
Alejandro Cebrián Sánchez <alejandrocebriansanchez@posteo.es>
2025-04-15 04:16:29 +0200

    Checkpoints added

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,17 +5,17 @@ public class BallController : MonoBehaviour {
     private Rigidbody rb;
 
     [SerializeField] private Transform multiTargetTransform;
-    private Vector3 localStartPosition;
+    private Vector3 respawnPosition;
     private float fallThresholdY = -1.5f;
 
-    void Start() {
+    void Awake() {
         rb = GetComponent<Rigidbody>();
 
         if (multiTargetTransform == null) {
             Debug.LogError("Debes asignar el MultiTarget en el Inspector.");
             return;
         }
-        localStartPosition = multiTargetTransform.InverseTransformPoint(transform.position);
+        respawnPosition = multiTargetTransform.InverseTransformPoint(transform.position);
     }
 
     void Update() {
@@ -33,7 +33,10 @@ public class BallController : MonoBehaviour {
     public void OnTriggerEnter(Collider other) {
         if (other.gameObject.name == "goal") {
             Debug.Log("¡Enhorabuena, has llegado a la meta!");
-        } else if (other.CompareTag("Hazard")) {
+        } else if (other.gameObject.CompareTag("Checkpoint")) {
+            respawnPosition = multiTargetTransform.InverseTransformPoint(other.transform.position);
+            Debug.Log("Checkpoint alcanzado.");
+        } else if (other.gameObject.CompareTag("Hazard")) {
             Debug.Log("Has tocado un peligro. Reiniciando posición...");
             ResetBall();
         }
@@ -42,7 +45,7 @@ public class BallController : MonoBehaviour {
     private void ResetBall() {
         rb.linearVelocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
-        transform.position = multiTargetTransform.TransformPoint(localStartPosition);
+        transform.position = multiTargetTransform.TransformPoint(respawnPosition);
     }
 
 }
--- Assets/Scripts/BumperController.cs
+++ Assets/Scripts/BumperController.cs
@@ -7,7 +7,7 @@ public class Bumper : MonoBehaviour
 	public int bumperForce = 800;
 	private GameObject player;
 
-	void Start () { player = GameObject.FindGameObjectWithTag("Player"); }
+	void Start() { player = GameObject.FindGameObjectWithTag("Player"); }
 
 	public void OnTriggerEnter(Collider collider) {
 		if (collider.gameObject == player) {
